8c24763602aa225ce8ea37df8961f71595eb783b
annguyen23 <54456351+annguyen23@users.noreply.github.com>
2021-10-09 13:23:39 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,14 +7,59 @@
  */
 public class Main {
 
+  /**
+   * This method print out number of *
+   * @param number: number of stars needed to print out
+   */
   public static void printStar(int number) {}
 
+ /**
+  * This method print out number of .
+  * @param number: number of dot needed to print out
+  */
   public static void printDot(int number) {}
 
+  /**
+   * This method print out the head of the arrow. 
+   * This is an example of head of arrow with size 7
+    ...*...
+    ..***..
+    .*****.
+    *******
+   * @param size: the width of the arrow
+   */
   public static void arrowHead(int size) {}
 
+  /**
+   * This method print out the trunk of the arrow.
+   * This is an example of trunk of arrow with size 7
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+   * @param size: the width of the arrow
+   */
   public static void arrowTrunk(int size) {}
 
+  /**
+   * This method print out the whole of the arrow like:
+   * This is an example of the arrow with size 7
+    ...*...
+    ..***..
+    .*****.
+    *******
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+    ...*...
+   * @param size: the width of the arrow
+   */
   public static void arrow(int size) {
     // we can only do odd size arrows
     assert (size % 2 == 1);
